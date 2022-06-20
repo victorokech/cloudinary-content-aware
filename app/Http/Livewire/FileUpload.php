f6b3c8718cf9394e212838f45aad40cc9fc987a8
Victor Okech <vickesnet@gmail.com>
2022-06-20 15:09:58 +0300

    First commit

---
+++ app/Http/Livewire/FileUpload.php
@@ -0,0 +1,11 @@
+<?php
+	
+	namespace App\Http\Livewire;
+	
+	use Livewire\Component;
+	
+	class FileUpload extends Component {
+		public function render() {
+			return view('livewire.file-upload');
+		}
+	}
